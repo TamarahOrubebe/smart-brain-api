c9f4ebbb483acec73f1d92cb44ac6cf1b6f266de
Your Name <you@example.com>
2021-11-10 09:20:53 +0100

    updates

--- controllers/register.js
+++ controllers/register.js
@@ -1,11 +1,11 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
-  console.log(req.body);
+  
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
   }
   const hash = bcrypt.hashSync(password);
-  console.log(hash);
+  
     db.transaction(trx => {
       trx.insert({
         hash: hash,
--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
-const morgan = require('morgan');
+const morgan = require("morgan");
 
 const register = require("./controllers/register");
 const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
 const image = require("./controllers/image");
-const morgan = require('morgan');
+const auth = require("./controllers/authorization")
+
 const db = knex({
 	// connect to your own database here:
 	client: "pg",
